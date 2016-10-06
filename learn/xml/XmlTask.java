7c43699352aea65a831234ab07e5a5c4642f0e21
MaltsevaEkaterina <Maltseva.ma@yandex.ru>
2016-10-06 07:30:14 +0300

    added entry point

---
+++ learn/xml/XmlTask.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package po41.Maltseva.wdad.learn.xml;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Мрр
+ */
+public class XmlTask {
+    public int salaryAverage(){
+        
+    }
+    public int salaryAverage(String departmentName){
+        
+    }
+    public void setJobTitile(String firstName, String
+secondName, String newJobTitle){
+        
+    }
+    public void setSalary(String firstName, String
+secondName, int newSalary){
+        
+    }
+    public void fireEmployee(String firstName, String
+secondName){
+        
+    }
+}
